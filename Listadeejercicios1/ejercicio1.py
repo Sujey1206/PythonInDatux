cc140a79d43f400ba4d9b65da580f8f048ee44d0
Sujey1206 <97492744+Sujey1206@users.noreply.github.com>
2023-04-23 10:09:00 -0500

    Create ejercicio1.py

---
+++ Listadeejercicios1/ejercicio1.py
@@ -0,0 +1,11 @@
+#EJERCICIO 1
+nombre = input("Ingresa tu nombre completo: ")
+dni = input("Ingresa tu DNI:")
+edad = input("Ingresa tu edad: ")
+
+print("""----------------
+Mi nombre es: {}
+Mi DNI es: {}
+Mi edad es: {} años
+----------------
+""".format(nombre, dni, edad))
