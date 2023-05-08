ed55bc7ba59b919eafb0ea30cb6dc09406bf5f0f
Sujey1206 <97492744+Sujey1206@users.noreply.github.com>
2023-05-07 21:04:14 -0500

    Create main.py

---
+++ Listadejercicios3/Ejercicio05/main.py
@@ -0,0 +1,20 @@
+import os
+
+class Producto:
+    def __init__(self, nombre, codigo):
+        self.nombre = nombre
+        self.codigo = codigo
+
+    def __str__(self):
+        pais, lote, anio = self.codigo.split("-")
+        return f"Producto: {self.nombre}\nPaís de origen: {pais}\nNúmero de lote: {lote}\nAño: {anio}"
+
+try:
+    from catalogo import Catalogo, Producto    
+    print("Funcion y clases importadas correctamente.")
+except ImportError:
+    print("Error al importar funciones o clases.")
+else:
+    print(f"Directorio de trabajo: {os.getcwd()}")
+finally:
+    print("Proceso terminado.")
